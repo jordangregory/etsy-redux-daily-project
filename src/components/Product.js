863d7dbaa9c0a87038d9e13cba0b050a4d848e0e
Jordan Gregory <jordangregory@gmail.com>
2017-08-03 15:20:38 -0500

    complete

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Product extends Component {
-    render() {
-        const {product} = this.props;
+  render() {
+    const { product } = this.props;
 
-        return (
-            <li>
-              <img src={product.image}></img>
-              <h3>{product.title}</h3>
-              <h4>{product.price}</h4>
-            </li>
-        )
-    }
+    return (
+      <li>
+        <img src={product.image} />
+        <h3>
+          {product.title}
+        </h3>
+        <h4>
+          {product.price}
+        </h4>
+      </li>
+    );
+  }
 }
 
 export default Product;
